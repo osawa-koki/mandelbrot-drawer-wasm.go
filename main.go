65335a7ea47aab77ca73c37997c5f52735cec93d
osawa-koki <koki@osawa.dev>
2023-10-26 21:31:35 +0900

    WASMでマンデルブロ集合を描写する。

--- main.go
+++ main.go
@@ -1,27 +1,66 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
   "fmt"
-  "syscall/js"
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"syscall/js"
 )
 
-func concat(_ js.Value, args []js.Value) any {
-  var result string
-  for _, arg := range args {
-    result += arg.String()
-  }
-  return result
+const (
+	xmin, ymin, xmax, ymax = -2, -2, +2, +2
+	width, height          = 1024, 1024
+	iterations             = 200
+	contrast               = 15
+)
+
+func drawMandelbrot(_this js.Value, _args []js.Value) any {
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+
+	bytes := image2bytes(img)
+	return bytes2base64(bytes)
 }
 
-func getElementById(_ js.Value, args []js.Value) any {
-  id := args[0].String()
-  return js.Global().Get("document").Call("getElementById", id)
+func image2bytes(img image.Image) []byte {
+	var buf bytes.Buffer
+	err := png.Encode(&buf, img)
+	if err != nil {
+		panic(err)
+	}
+	return buf.Bytes()
+}
+
+func bytes2base64(bytes []byte) string {
+	return base64.StdEncoding.EncodeToString(bytes)
+}
+
+func mandelbrot(z complex128) color.Color {
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
 }
 
 func main() {
-	fmt.Println("Go program started...")
-  js.Global().Set("concat", js.FuncOf(concat))
-  js.Global().Set("getElementById", js.FuncOf(getElementById))
-  fmt.Println("Go funcitons setup complete.")
+  fmt.Println("Program started!")
+	js.Global().Set("drawMandelbrot", js.FuncOf(drawMandelbrot))
+	fmt.Println("Program finished!")
   select {}
 }
